9e15a37c0d0f8be5d3dde39571caf1485c2d235a
MilkyWay <vinogradov.e@indriver.com>
2022-03-28 12:36:10 +1000

    init commit Base functional

---
+++ hw03_frequency_analysis/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	hw03frequencyanalysis "github.com/MilkyWay-core/otus_golang/hw03_frequency_analysis"
+)
+
+func main() {
+	var text string
+	fmt.Println("Enter you text heare: ")
+	consoleReader := bufio.NewReader(os.Stdin)
+	text, _ = consoleReader.ReadString('\n')
+	words := hw03frequencyanalysis.Top10(text)
+	for _, word := range words {
+		fmt.Println(word)
+	}
+
+}
--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,60 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type wordCounted struct {
+	count uint
+	word  string
+}
+
+type countedList struct {
+	words []wordCounted
+}
+
+func constructor(cl countedList) countedList {
+	cl.words = make([]wordCounted, 0)
+	return cl
+}
+
+func (cl countedList) add(wc wordCounted) countedList { //Was adding only uniq word
+	for _, wordCounter := range cl.words {
+		if wordCounter.word == wc.word {
+			return cl
+		}
+	}
+	cl.words = append(cl.words, wc)
+	return cl
+}
+
+func Top10(str string) []string {
+	fmt.Println("start")
+	words := strings.Fields(str)
+	var resultArray []string
+	var cl countedList = constructor(countedList{})
+	for i, word := range words { // will counted  words in string
+		var counterWord uint
+		for n := i; n < len(words); n++ {
+			if word == words[n] {
+				counterWord++
+			}
+		}
+		cl = cl.add(wordCounted{counterWord, word})
+	}
+	sort.SliceStable(cl.words, func(i, j int) bool { //sort in structure
+		return cl.words[i].count > cl.words[j].count || (cl.words[i].count == cl.words[j].count && cl.words[i].word < cl.words[j].word)
+	})
+	fmt.Println(cl.words)
+	for i, word := range cl.words { //convert structure to string array
+		if i > 9 {
+			break
+		}
+		resultArray = append(resultArray, word.word)
+	}
+
+	return resultArray
+
 }
